a70ee25e5268a1f624dabae8466998009f5c6258
Rozagami <klaudiaxkulpa@gmail.com>
2025-03-06 20:38:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ scene.add(directionalLight);
 const loader = new THREE.GLTFLoader();
 let model;
 
-loader.load('models/model.glb?v=6', function (gltf) { // Nowa wersja modelu
+loader.load('models/model_v2.glb', function (gltf) {
     console.log("✅ Model załadowany! 🎉");
     model = gltf.scene;
 
