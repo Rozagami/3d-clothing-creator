89348ef59e445bba482081f10178d98a962b96d6
Rozagami <klaudiaxkulpa@gmail.com>
2025-03-06 01:30:04 +0100

    3d

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Importujemy GLTFLoader poprawnie jako moduł
+// Importujemy Three.js oraz GLTFLoader poprawnie jako moduły
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Inicjalizacja sceny
